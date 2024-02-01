2600021f509c0f1d15ac391eea5f5344661e0e55
arif20026 <arifislamipe026@gmail.com>
2024-02-01 12:31:56 +0600

    added filter functionality for asset based on type

--- src/Pages/Dashboard/RequestAsset/RequestAsset.jsx
+++ src/Pages/Dashboard/RequestAsset/RequestAsset.jsx
@@ -6,22 +6,53 @@ const RequestAsset = () => {
 
     const [summaryData, setSummaryData] = useState([]);
 
+    const [assetTypeFilter, setAssetTypeFilter] = useState('');
+    const [_id, set_id] = useState('');
+
+
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        // Fetch data from the API endpoint
-        fetch('http://localhost:5000/api/assetSum')
+    const fetchSummaryData = () => {
+        fetch(`http://localhost:5000/api/assetSum?assetType=${assetTypeFilter}&_id=${_id}`)
             .then(response => response.json())
             .then(data => setSummaryData(data))
             .catch(error => console.error(error));
-    }, []);
+    };
 
-    console.log(summaryData)
+    useEffect(() => {
+        fetchSummaryData();
+    }, [ assetTypeFilter,_id]);
+
+    const handleSearch = () => {
+        fetchSummaryData();
+    };
 
     return (
         <div>
 
             <h3 className="font-bold text-4xl text-center">Asset List Section</h3>
+
+            <label>
+                <span className="font-bold">Filter by Type:</span> 
+                <select value={assetTypeFilter} onChange={(e) => setAssetTypeFilter(e.target.value)}>
+                    <option value="">All</option>
+                    <option value="Returnable">Returnable</option>
+                    <option value="Nonreturnable">Non Returnable</option>
+                </select>
+            </label>
+
+            <div className="my-4">
+                <label>
+                    <input type="text" value={_id} onChange={(e) => set_id(e.target.value)} placeholder="Search by Product Name" className="border border-black" />
+                </label>
+                <button className="btn btn-primary btn-sm mx-4" onClick={handleSearch}>
+                    Search 
+                </button>
+            </div>
+
+            {/* <button className="btn btn-primary" onClick={handleFilter}>
+                    Apply Filter
+                </button> */}
 
             {
 
@@ -42,23 +73,23 @@ const RequestAsset = () => {
                                 <dialog id="my_modal_1" className="modal">
                                     <div className="modal-box">
                                         <div className="modal-action">
-                                        
+
 
                                             <div className="flex flex-col space-y-8">
 
-                                            <form className="space-x-8">
-                                                <input type="text" placeholder="Additional notes" className="border border-black"/>
-                                                <input type="submit"  className="btn btn-primary" value="Request" />
-                                            </form>
+                                                <form className="space-x-8">
+                                                    <input type="text" placeholder="Additional notes" className="border border-black" />
+                                                    <input type="submit" className="btn btn-primary" value="Request" />
+                                                </form>
 
-                                            <form method="dialog">
-                                                {/* if there is a button in form, it will close the modal */}
-                                                <button className="btn">Close</button>
-                                            </form>
+                                                <form method="dialog">
+                                                    {/* if there is a button in form, it will close the modal */}
+                                                    <button className="btn">Close</button>
+                                                </form>
 
                                             </div>
-                                           
-                                            
+
+
                                         </div>
                                     </div>
                                 </dialog>
